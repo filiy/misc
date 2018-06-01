6fb38420a60d781dff75b6e8d423b4374a2d4e96
phil <phil.thomson@arctiq.ca>
2018-06-01 08:45:54 -0700

    adding in file

--- ps -C -1 -O rss - gawk - count - sum - -2 - END -count --- print -Number of processes -count- print -Memory usage per process -sum/1024/count- -MB- print -Total memory usage - sum/psmem.sh
+++
@@ -1,5 +0,0 @@
-#Stolen from:
-#https://abdussamad.com/archives/488-Memory-usage-of-a-process-under-Linux.html
-
-#!/bin/bash
-ps -C $1 -O rss | gawk '{ count ++; sum += $2 }; END {count --; print "Number of processes =",count; print "Memory usage per process =",sum/1024/count, "MB"; print "Total memory usage =", sum/1024, "MB" ;};'
