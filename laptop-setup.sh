d541e1f0e115a190a6bbf49ee6523060557bb58e
Phil Thomson <thomson.p@gmail.com>
2020-10-11 10:44:22 -0700

    Create laptop-setup.sh

---
+++ laptop-setup.sh
@@ -0,0 +1,94 @@
+yum install -y 
+
+
+
+#tweaks
+alternatetab
+applications menu
+dash to panel
+desktop icons
+openweather
+tray icons
+user themes
+
+#vpn
+telus vpn
+red hat vpn
+artiqlab vpn
+chassi vpn
+
+#firefox extensions
+adblock
+dark
+user-agent-switcher
+https://mycroftproject.com/google-search-plugins.html
+
+#apps
+vscode
+terraform
+packer
+minishift
+thunar
+@virtualization
+oc
+gnome-tweak-tool
+pip install awscli
+gcloud
+docker
+rocketchat
+screen
+vault
+vim
+azure-cli
+peek
+emberthemilk
+evernote
+keepassx
+chromium
+obs-studio
+numix-gtk-theme
+numix-icon-theme
+tlp
+kubectl
+conky
+synergy
+gimp
+bluejeans
+falkon
+remmina
+audacity
+obs
+dive
+skype
+weechat
+transmission
+jmeter
+slack
+teamviewer
+wireshark
+conly-manager
+zoom
+rtm
+vlc
+hexhat
+google music player
+cockpit
+gparted
+scap-workbench
+powertop
+ntsysv
+strongswan
+git 
+ansible
+glances
+fuse-exfat
+jq
+skopeo
+iptraf
+expect
+unrar
+xclip
+meld
+dive
+fuse-exfat
+draw
