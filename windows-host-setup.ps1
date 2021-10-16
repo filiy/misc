c8f91e791980a6f85e2b1d000737dc47d54eb010
Phil Thomson <thomson.p@gmail.com>
2021-10-16 13:16:36 -0700

    Create windows-host-setup.ps1

---
+++ windows-host-setup.ps1
@@ -0,0 +1,6 @@
+#powershell.exe -ExecutionPolicy ByPass -File "windows-host-setup.ps1"
+
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+powershell.exe -ExecutionPolicy ByPass -File $file
